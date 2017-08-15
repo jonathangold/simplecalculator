5df3bc6e4fa44e5cc3cbd067c84b41ca685ec7ff
jonathangold <jonathan.e.gold@gmail.com>
2017-08-15 20:27:05 +0000

    fixed issue preventing calculations with result after equals

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 // Initialize app
 var myApp = new Framework7();
 
@@ -7,102 +8,108 @@ var $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+	// Because we want to use dynamic navbar, we need to enable it for this view:
+	dynamicNavbar: true
 });
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-  calcModule.init()
+	calcModule.init()
 });
 
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
 myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
+	// Do something here for "about" page
 
 })
 
 let calcModule = {
-  buffer: '0',
-  result: 0,
-  newln: true ,
-  oper: '+',
+	buffer: '0',
+	result: 0,
+	newln: true ,
+	oper: '+',
 
-  init: function(){
-    this.bindEvents()
-  },
+	init: function(){
+		this.bindEvents()
+	},
 
-  bindEvents: function(){
-    $$('.number').on('click', function(event){
-        let value = $$(event.target).text()
-        calcModule.numInput(value)
-    })
-    $$('.oper').on('click', function(event){
-        let value = $$(event.target).text()
-        calcModule.opInput(value)
-    })
-    $$('.decimal').on('click', function(event){
-        let value = $$(event.target).text()
-        calcModule.decInput()
-    })
-    $$('.clear').on('click', function(event){
-        let value = $$(event.target).text()
-        calcModule.clear(value)
-    })
+	bindEvents: function(){
+		$$('.number').on('click', function(event){
+			let value = $$(event.target).text()
+			calcModule.numInput(value)
+		})
+		$$('.oper').on('click', function(event){
+			let value = $$(event.target).text()
+			calcModule.opInput(value)
+		})
+		$$('.decimal').on('click', function(event){
+			let value = $$(event.target).text()
+			calcModule.decInput()
+		})
+		$$('.clear').on('click', function(event){
+			let value = $$(event.target).text()
+			calcModule.clear(value)
+		})
 
-  },
+	},
 
-  numInput: function(value){
-    if (this.newln){
-      $$('.card').text(value)
-      this.newln = false
-    }
-    else {
-    $$('.card').append(value)
-    }
-  },
+	numInput: function(value){
+		if (this.newln && this.oper == '='){
+			this.result = 0
+			this.oper = '+'
+		}
+		if (this.newln){
+			$$('.card').text(value)
+			this.newln = false
+		}
+		else {
+			$$('.card').append(value)
+		}
+	},
 
-  opInput: function(value){
-    if (!this.newln){
-      let buff = $$('.card').text()
-      this.putBuffer(buff + value)
-      this.newln = true
-      this.getResult()
-    }
-  },
+	opInput: function(value){
+		if (this.newln && this.oper == '='){
+			this.buffer = this.buffer.slice(0 , -1) + value
+			this.oper = value
+			this.newln = true
+		}
+		if (!this.newln){
+			let buff = $$('.card').text()
+			this.putBuffer(buff + value)
+			this.newln = true
+			this.getResult()
+		}
+	},
 
-  decInput: function() {
-    if (this.newln == true) {
-      $$('.card').text('0')
-    }
-    if (!$$('.card').text().includes('.')) {
-      $$('.card').text($$('.card').text() + '.')
-      this.newln = false
-    }
-  },
+	decInput: function() {
+		if (this.newln) {
+			$$('.card').text('0')
+		}
+		if (!$$('.card').text().includes('.')) {
+			$$('.card').text($$('.card').text() + '.')
+			this.newln = false
+		}
+	},
 
-  putBuffer: function(e){
-    if (this.oper == '='){
-      this.result = 0
-      this.oper = '+'
-    }
-    this.buffer = this.result + this.oper + e
-    this.oper = e.slice(-1)
-  },
+	putBuffer: function(e){
 
-  getResult: function(){
-    this.result = eval(this.buffer.slice(0, -1))
-    $$('.card').text(this.result)
-  },
+		this.buffer = this.result + this.oper + e
+		this.oper = e.slice(-1)
+	},
 
-  clear: function(value){
-    $$('.card').text('0')
-    this.newln = true
-    if (value == 'CE'){
-      this.result = 0
-      this.oper = '+'
-    }
-  }
+	getResult: function(){
+		this.result = eval(this.buffer.slice(0, -1))
+		$$('.card').text(this.result)
+	},
+
+	clear: function(value){
+		$$('.card').text('0')
+		this.newln = true
+		if (value == 'CE'){
+			this.result = 0
+			this.oper = '+'
+		}
+	}
 }
